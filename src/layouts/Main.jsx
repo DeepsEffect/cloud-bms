e57071fd4710bd438b35eba090d7d4b729655aca
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-06-03 22:47:11 +0600

    footer added

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Nav from "../pages/Shared/Nav/Nav";
+import Footer from "../pages/Shared/Footer/Footer";
 
 const Main = () => {
   return (
     <div>
       <Nav />
-      <div>
+      <div className="container mx-auto min-h-[calc(100svh-72px)]">
         <Outlet />
       </div>
+      <Footer />
     </div>
   );
 };
--- src/routers/routes.jsx
+++ src/routers/routes.jsx
@@ -1,16 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../pages/Home/Home/Home";
+import Error from "../pages/Error/Error";
+import Login from "../pages/Login/Login";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
     children: [
-        {
-            index: true,
-            element: <Home />
-        }
-    ]
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <Error />,
   },
 ]);
