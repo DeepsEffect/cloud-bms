4535d9a32d3858e5e1645875e1e5b82095f6c6f8
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-06-04 19:54:26 +0600

    added aprtment title

--- src/pages/Apartment/Apartment.jsx
+++ src/pages/Apartment/Apartment.jsx
@@ -1,7 +1,16 @@
 const Apartment = () => {
     return (
         <div>
-            <h2>apartment</h2>
+           {/* title */}
+           <section className="text-center mt-10 max-w-md mx-auto">
+            <h2 className="text-2xl font-bold font-heading">All available rooms</h2>
+            <p>choose and find your rooms that suits you, available with beautiful views</p>
+           </section>
+
+        {/* cards */}
+        <section>
+            
+        </section>
         </div>
     );
 };
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -148,7 +148,7 @@ const SignUp = () => {
             </div>
             <button
               type="submit"
-              className="mt-6 rounded-lg bg-primary-600 px-4 py-2 text-center text-base font-semibold text-white shadow-md outline-none ring-primary-500 ring-offset-2 transition hover:bg-primary-700 focus:ring-2 md:w-32"
+              className="mt-6 w-full rounded-lg bg-primary-600 px-4 py-2 text-center text-base font-semibold text-white shadow-md outline-none ring-primary-500 ring-offset-2 transition hover:bg-primary-700 focus:ring-2 "
             >
               Sign in
             </button>
