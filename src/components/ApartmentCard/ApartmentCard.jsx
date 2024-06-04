f8341f96d1fd90a8b956d60f3341cedb4052838d
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-06-05 01:08:46 +0600

    added apartmentCard and pagination

--- src/components/ApartmentCard/ApartmentCard.jsx
+++ src/components/ApartmentCard/ApartmentCard.jsx
@@ -12,23 +12,36 @@ const ApartmentCard = ({ room }) => {
     <Card className="mt-6 lg:w-96">
       <CardHeader color="blue-gray" className="relative h-56">
         <img
-          src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
+          className="object-cover object-center h-full w-full"
+          src={room.image}
           alt="card-image"
         />
       </CardHeader>
       <CardBody>
-        <section className="grid grid-cols-2 text-center">
+        <section className="grid grid-cols-1 gap-2 lg:grid-cols-2 lg:font-bold">
           <h2 color="blue-gray" className="mb-2 ">
-            Floor No: {room.floor_no}
+            Floor No:{" "}
+            <span className="ml-2 mr-3 rounded-full bg-orange-100 px-2 py-0.5 text-orange-900">
+              {room.floor_no}
+            </span>
           </h2>
           <h2 color="blue-gray" className="mb-2 ">
-            Block Name: {room.block_name}
+            Block Name:{" "}
+            <span className="ml-2 mr-3 rounded-full bg-blue-100 px-2 py-0.5 text-blue-900">
+              {room.block_name}
+            </span>
           </h2>
           <h2 color="blue-gray" className="mb-2 ">
-            Apartment No: {room.apartment_no}
+            Apartment:{" "}
+            <span className="ml-2 mr-3 rounded-full bg-accent-100 px-2 py-0.5 text-accent-900">
+              {room.apartment_no}
+            </span>
           </h2>
           <h2 color="blue-gray" className="mb-2 ">
-            Rent: ${room.rent}
+            Rent:{" "}
+            <span className="ml-2 mr-3 rounded-full bg-green-100 px-2 py-0.5 text-green-900">
+              ${room.rent}
+            </span>
           </h2>
         </section>
       </CardBody>
