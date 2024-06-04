16f6cf313f9f9c98369627caa762f5848c13d6b4
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-06-04 21:28:50 +0600

    axiosPublic hook and load data using tanstack

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,19 @@ import { router } from "./routers/routes.jsx";
 import { RouterProvider } from "react-router-dom";
 import AuthProvider from "./providers/AuthProvider.jsx";
 import { Toaster } from "react-hot-toast";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
     <Toaster />
   </React.StrictMode>
 );
--- src/pages/Apartment/Apartment.jsx
+++ src/pages/Apartment/Apartment.jsx
@@ -1,18 +1,32 @@
-const Apartment = () => {
-    return (
-        <div>
-           {/* title */}
-           <section className="text-center mt-10 max-w-md mx-auto">
-            <h2 className="text-2xl font-bold font-heading">All available rooms</h2>
-            <p>choose and find your rooms that suits you, available with beautiful views</p>
-           </section>
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
 
-        {/* cards */}
-        <section>
-            
-        </section>
-        </div>
-    );
+const Apartment = () => {
+  const axiosPublic = useAxiosPublic();
+  const { data: apartments = [] } = useQuery({
+    queryKey: ["apartments"],
+    queryFn: async () => {
+      const res = await axiosPublic("/apartments");
+      return res.data;
+    },
+  });
+  console.log(apartments);
+
+  return (
+    <div>
+      {/* title */}
+      <section className="text-center mt-10 max-w-md mx-auto">
+        <h2 className="text-2xl font-bold font-heading">All available rooms</h2>
+        <p>
+          choose and find your rooms that suits you, available with beautiful
+          views
+        </p>
+      </section>
+
+      {/* cards */}
+      <section></section>
+    </div>
+  );
 };
 
 export default Apartment;
