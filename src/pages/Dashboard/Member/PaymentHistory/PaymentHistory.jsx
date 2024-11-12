051543371c464e18d2c93b3e4cb484a60f419e84
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-11-12 21:39:21 +0600

    added payment history table

--- src/pages/Dashboard/Member/PaymentHistory/PaymentHistory.jsx
+++ src/pages/Dashboard/Member/PaymentHistory/PaymentHistory.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import useAxiosSecure from "../../../../hooks/useAxiosSecure";
 import DashboardTitle from "../../Common/DashboardTitle/DashboardTitle";
+import useAuth from "../../../../hooks/useAuth";
 
 const PaymentHistory = () => {
   const [payments, setPayments] = useState([]);
+  const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
   useEffect(() => {
     const fetchPayments = async () => {
@@ -16,13 +18,77 @@ const PaymentHistory = () => {
     };
     fetchPayments();
   }, [axiosSecure]);
-  console.log(payments);
+
+  //   console.log(payments);
+  const usersPayment = payments.filter(
+    (payment) => payment?.email === user?.email
+  );
+  //   console.log(payments);
+  //   console.log(usersPayment);
+  const TABLE_HEAD = ["Email", "Amount", "Transactor Id", "Date", "Status"];
   return (
     <div>
       <DashboardTitle title={"payment history"} />
-      <h2 className="text-xl font-heading text-center mt-10">
-        There is no history yet...
-      </h2>
+      {usersPayment === 0 ? (
+        <h2 className="text-xl font-heading text-center mt-10">
+          There is no history yet...
+        </h2>
+      ) : (
+        <section className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md h-full w-full overflow-scroll lg:overflow-hidden">
+          <table className="w-full min-w-max table-auto text-left overflow-x-auto">
+            <thead>
+              <tr>
+                {TABLE_HEAD.map((head, idx) => (
+                  <th
+                    key={idx}
+                    className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
+                  >
+                    <div
+                      color="blue-gray"
+                      className="font-normal leading-none opacity-70"
+                    >
+                      {head}
+                    </div>
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {usersPayment.map(({ email, transactionId, date, price }) => (
+                //   index
+                <tr
+                  key={transactionId}
+                  className="even:bg-blue-gray-50/50 overflow-hidden"
+                >
+                  <td className="p-4">
+                    <div color="blue-gray" className="font-normal">
+                      {email}
+                    </div>
+                  </td>
+                  <td className="p-4">
+                    <div color="blue-gray" className="font-normal">
+                      ${price}
+                    </div>
+                  </td>
+                  <td className="p-4">
+                    <div color="blue-gray" className="font-normal">
+                      {transactionId}
+                    </div>
+                  </td>
+                  <td className="p-4">
+                    <div color="blue-gray" className="font-normal">
+                      {new Date(date).toLocaleDateString()}
+                    </div>
+                  </td>
+                  <td className="p-4">
+                    <div className="font-normal text-green-400">paid</div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </section>
+      )}
     </div>
   );
 };
