a065f0ebe31c67ef561c330117810cb025c46225
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-11-12 22:27:52 +0600

    availavle and unavailable apartmens percantage counts

--- src/pages/Dashboard/Admin/AdminProfile/AdminProfile.jsx
+++ src/pages/Dashboard/Admin/AdminProfile/AdminProfile.jsx
@@ -31,11 +31,34 @@ const AdminProfile = () => {
     },
   });
 
+  // total agreements
+  const { data: agreements = [] } = useQuery({
+    queryKey: ["agreements"],
+    queryFn: async () => {
+      const { data } = await axiosSecure(`/agreements`);
+      return data;
+    },
+  });
+
+  const totalApartments = apartments?.length || 0;
+  const agreementsCount = agreements?.length || 0;
+
+  // Calculate percentages
+  const availablePercentage =
+    ((totalApartments - agreementsCount) / totalApartments) * 100;
+  const unavailablePercentage = (agreementsCount / totalApartments) * 100;
+
   return (
     <div className="flex flex-wrap gap-4 lg:gap-8 justify-center bg-gray-100 py-10 lg:p-14 overflow-auto">
-      <InfoCard title={"total rooms"} desc={apartments?.length} />
-      <InfoCard title={"Available Rooms"} desc={"45%"} />
-      <InfoCard title={"Unavailable Rooms"} desc={"55%"} />
+      <InfoCard title={"Total Apartments"} desc={apartments?.length} />
+      <InfoCard
+        title={"Available Apartments"}
+        desc={`${Math.floor(availablePercentage)}%`}
+      />
+      <InfoCard
+        title={"Unavailable Apartments"}
+        desc={`${Math.floor(unavailablePercentage)}%`}
+      />
       <InfoCard title={"Total Users"} desc={users?.length} />
       <InfoCard title={"Total Members"} desc={members?.length} />
     </div>
