68580706a6ff10f3038208a1ab32bfbf00aded49
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-11-13 11:03:32 +0600

    dynamic coupon fetching and showing in the ui

--- src/pages/Home/Coupon/Coupon.jsx
+++ src/pages/Home/Coupon/Coupon.jsx
@@ -1,39 +1,60 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CopyToClipboard from "react-copy-to-clipboard";
 import toast from "react-hot-toast";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+
 const Coupon = () => {
   const [copied, setCopied] = useState(false);
+  const [coupons, setCoupons] = useState([]);
+  const axiosSecure = useAxiosSecure();
 
   const handleCopy = () => {
     setCopied(true);
-    toast.success('text copied')
+    toast.success("text copied");
     setTimeout(() => setCopied(false), 4000);
   };
 
+  // fetch coupons from db
+  useEffect(() => {
+    const fetchCoupons = async () => {
+      const res = await axiosSecure("/coupons");
+      if (res.status === 200) {
+        setCoupons(res.data);
+      }
+    };
+    fetchCoupons();
+  }, [axiosSecure]);
+
+  // Get the latest coupon
+  const latestCoupon = coupons
+    .slice()
+    .sort((a, b) => new Date(b.dataAdded) - new Date(a.dataAdded))[0];
+
+  // console.log(latestCoupon);
   return (
     // <!-- component -->
     <div className="lg:mt-10">
       <div className="bg-gradient-to-br from-primary-600 to-accent-600 text-white text-center py-10 px-20 rounded-lg shadow-md relative">
         <h2 className="max-w-lg mx-auto mb-4 rounded-lg text-xl">
-          Summer sale offer!
+          Winter&apos;s cold offer!
         </h2>
         <h3 className=" text-xl lg:text-2xl font-semibold mb-4">
-          20% off if you apply the coupon below!
+          {latestCoupon?.desc}
         </h3>
         <div className="flex items-center space-x-2 mb-6">
           <span
             id="cpnCode"
             className="border-dashed border text-white px-4 py-2 rounded-l"
           >
-            could-stinger
+            {latestCoupon?.code}
           </span>
-          <CopyToClipboard text="could-stinger" onCopy={handleCopy}>
+          <CopyToClipboard text={latestCoupon?.code} onCopy={handleCopy}>
             <span className="border border-white bg-white text-primary-600 px-4 py-2 rounded-r cursor-pointer">
               {copied ? "Copied!" : "Copy Code"}
             </span>
           </CopyToClipboard>
         </div>
-        <p className="text-sm">Valid Till: 4Dec, 2024</p>
+        <p className="text-sm">Valid Till: 30Dec, 2024</p>
 
         <div className="lg:w-12 lg:h-12 bg-white rounded-full absolute top-1/2 transform -translate-y-1/2 left-0 -ml-6"></div>
         <div className="lg:w-12 lg:h-12 bg-white rounded-full absolute top-1/2 transform -translate-y-1/2 right-0 -mr-6"></div>
